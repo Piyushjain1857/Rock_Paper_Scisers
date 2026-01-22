f11ed9e136584e331fdec3968383d513164cc288
Piyush <piyushjain1857@gmail.com>
2026-01-22 20:27:33 +0530

    added 2 more varialbles

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const choices = document.querySelectorAll(".choice");
 const userChoiceDisplay = document.getElementById("user-choice");
 const computerChoiceDisplay = document.getElementById("computer-choice");
 const resultText = document.getElementById("result-text");
+const resetBtn = document.getElementById("reset");
+const userScoreDisplay = document.getElementById("user-score");
