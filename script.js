2ba4cf51c37a816ce602956af3d74574afa87248
Piyush <piyushjain1857@gmail.com>
2026-01-22 20:27:17 +0530

    added varialbles

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 const choices = document.querySelectorAll(".choice");
 const userChoiceDisplay = document.getElementById("user-choice");
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const resultText = document.getElementById("result-text");
